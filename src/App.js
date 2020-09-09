bd6ff1a4061cae624428189142785c61ca15946b
Streed <dieppois.streed@gmail.com>
2020-09-09 15:06:07 +0200

    First React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ShoppingApp from "./components/ShoppingApp";
 
 function App() {
   return (
-    <div className="App">
+    <div className="container my-3">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Ma liste de course</h1>
+        <p>On va faire une shopping list</p>
+        <ShoppingApp />
       </header>
     </div>
   );
